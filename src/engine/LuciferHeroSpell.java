d71344e1b6d370854ee05326ad0337a3699ed1c5
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-06-27 00:40:26 +0200

    Work on #1 for LuciferHeroSpell and create correct Constructors.

--- src/engine/LuciferHeroSpell.java
+++ src/engine/LuciferHeroSpell.java
@@ -2,11 +2,36 @@ package engine;
 
 import java.io.IOException;
 
+/**
+ * @author alina
+ *
+ * This class represents a Spell of an Hero of the RPG-Maker-Game.
+ */
 public class LuciferHeroSpell {
 	public long level	= 1;
 	public long spell	= 1;
 	
-	public LuciferHeroSpell(DataReader sr) throws IOException {
+	/**
+	 * Constructs a new LuciferHeroSpell
+	 * 
+	 * @param bytes byte-Array which represents the LuciferHeroSpell
+	 * @throws IOException is thrown on any parsing-error
+	 */
+	public LuciferHeroSpell(byte[] bytes) throws IOException {
+		init(new DataReader(bytes));
+	}
+	
+	/**
+	 * Constructs a new LuciferHeroSpell
+	 * 
+	 * @param dr DataReader which represents the LuciferHeroSpell
+	 * @throws IOException is thrown on any parsing-error
+	 */
+	public LuciferHeroSpell(DataReader dr) throws IOException {
+		init(dr);
+	}
+	
+	private void init(DataReader sr) throws IOException {
 		LuciferBaseUnit unit = sr.nextUnit();
 		while (unit.id != 0) {
 			switch (unit.id) {
