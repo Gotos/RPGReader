c0bfe92cb09d7f6559b84605464a0d018ebb6454
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-07-20 14:05:17 +0200

    Work on #8 for LuciferMapEventPage

--- src/engine/LuciferMapEventPage.java
+++ src/engine/LuciferMapEventPage.java
@@ -11,23 +11,308 @@ import java.util.ArrayList;
  */
 public class LuciferMapEventPage {
 	
-	public LuciferMapEventPageCondition conditions		= new LuciferMapEventPageCondition();
-	public String charset								= "";
-	public long graphic								= 0;
-	public long facing									= -1;
-	public long stepping								= 1;
-	public long movementType							= 0;
-	public long movementFrequency						= 3;
-	public long eventStartCondition					= 0;
-	public long position								= 0;
-	public long animationType							= 0;
-	public long movementSpeed							= 2;
+	private LuciferMapEventPageCondition conditions		= new LuciferMapEventPageCondition();
+	private String charset								= "";
+	private long graphic								= 0;
+	private long facing									= -1;
+	private long stepping								= 1;
+	private long movementType							= 0;
+	private long movementFrequency						= 3;
+	private long eventStartCondition					= 0;
+	private long position								= 0;
+	private long animationType							= 0;
+	private long movementSpeed							= 2;
 	private long scriptLength							= 0;
-	public LuciferRoute route							= new LuciferRoute();
-	public ArrayList<LuciferEventCommand> commands		= new ArrayList<LuciferEventCommand>();
-	public boolean transparent							= false;
-	public boolean preventEventOverlap					= false;
+	private LuciferRoute route							= new LuciferRoute();
+	private ArrayList<LuciferEventCommand> commands		= new ArrayList<LuciferEventCommand>();
+	private boolean transparent							= false;
+	private boolean preventEventOverlap					= false;
 	
+	/**
+	 * Returns the conditions
+	 * 
+	 * @return the conditions
+	 */
+	public LuciferMapEventPageCondition getConditions() {
+		return conditions;
+	}
+
+	/**
+	 * Sets the conditions
+	 * 
+	 * @param conditions the new conditions
+	 */
+	public void setConditions(
+			LuciferMapEventPageCondition conditions) {
+		this.conditions = conditions;
+	}
+
+	/**
+	 * Returns the charset
+	 * 
+	 * @return the charset
+	 */
+	public String getCharset() {
+		return charset;
+	}
+
+	/**
+	 * Sets the charset
+	 * 
+	 * @param charset the new charset
+	 */
+	public void setCharset(
+			String charset) {
+		this.charset = charset;
+	}
+
+	/**
+	 * Returns the graphic
+	 * 
+	 * @return the graphic
+	 */
+	public long getGraphic() {
+		return graphic;
+	}
+
+	/**
+	 * Sets the graphic
+	 * 
+	 * @param graphic the new graphic
+	 */
+	public void setGraphic(
+			long graphic) {
+		this.graphic = graphic;
+	}
+
+	/**
+	 * Returns the facing
+	 * 
+	 * @return the facing
+	 */
+	public long getFacing() {
+		return facing;
+	}
+
+	/**
+	 * Sets the facing
+	 * 
+	 * @param facing the new facing
+	 */
+	public void setFacing(
+			long facing) {
+		this.facing = facing;
+	}
+
+	/**
+	 * Returns the stepping
+	 * 
+	 * @return the stepping
+	 */
+	public long getStepping() {
+		return stepping;
+	}
+
+	/**
+	 * Sets the stepping
+	 * 
+	 * @param stepping the new stepping
+	 */
+	public void setStepping(
+			long stepping) {
+		this.stepping = stepping;
+	}
+
+	/**
+	 * Returns the movementType
+	 * 
+	 * @return the movementType
+	 */
+	public long getMovementType() {
+		return movementType;
+	}
+
+	/**
+	 * Sets the movementType
+	 * 
+	 * @param movementType the new movementType
+	 */
+	public void setMovementType(
+			long movementType) {
+		this.movementType = movementType;
+	}
+
+	/**
+	 * Returns the movementFrequency
+	 * 
+	 * @return the movementFrequency
+	 */
+	public long getMovementFrequency() {
+		return movementFrequency;
+	}
+
+	/**
+	 * Sets the movementFrequency
+	 * 
+	 * @param movementFrequency the new movementFrequency
+	 */
+	public void setMovementFrequency(
+			long movementFrequency) {
+		this.movementFrequency = movementFrequency;
+	}
+
+	/**
+	 * Returns the eventStartCondition
+	 * 
+	 * @return the eventStartCondition
+	 */
+	public long getEventStartCondition() {
+		return eventStartCondition;
+	}
+
+	/**
+	 * Sets the eventStartCondition
+	 * 
+	 * @param eventStartCondition the new eventStartCondition
+	 */
+	public void setEventStartCondition(
+			long eventStartCondition) {
+		this.eventStartCondition = eventStartCondition;
+	}
+
+	/**
+	 * Returns the position
+	 * 
+	 * @return the position
+	 */
+	public long getPosition() {
+		return position;
+	}
+
+	/**
+	 * Sets the position
+	 * 
+	 * @param position the new position
+	 */
+	public void setPosition(
+			long position) {
+		this.position = position;
+	}
+
+	/**
+	 * Returns the animationType
+	 * 
+	 * @return the animationType
+	 */
+	public long getAnimationType() {
+		return animationType;
+	}
+
+	/**
+	 * Sets the animationType
+	 * 
+	 * @param animationType the new animationType
+	 */
+	public void setAnimationType(
+			long animationType) {
+		this.animationType = animationType;
+	}
+
+	/**
+	 * Returns the movementSpeed
+	 * 
+	 * @return the movementSpeed
+	 */
+	public long getMovementSpeed() {
+		return movementSpeed;
+	}
+
+	/**
+	 * Sets the movementSpeed
+	 * 
+	 * @param movementSpeed the new movementSpeed
+	 */
+	public void setMovementSpeed(
+			long movementSpeed) {
+		this.movementSpeed = movementSpeed;
+	}
+
+	/**
+	 * Returns the route
+	 * 
+	 * @return the route
+	 */
+	public LuciferRoute getRoute() {
+		return route;
+	}
+
+	/**
+	 * Sets the route
+	 * 
+	 * @param route the new route
+	 */
+	public void setRoute(
+			LuciferRoute route) {
+		this.route = route;
+	}
+
+	/**
+	 * Returns the commands
+	 * 
+	 * @return the commands
+	 */
+	public ArrayList<LuciferEventCommand> getCommands() {
+		return commands;
+	}
+
+	/**
+	 * Sets the commands
+	 * 
+	 * @param commands the new commands
+	 */
+	public void setCommands(
+			ArrayList<LuciferEventCommand> commands) {
+		this.commands = commands;
+	}
+
+	/**
+	 * Returns the transparent
+	 * 
+	 * @return the transparent
+	 */
+	public boolean isTransparent() {
+		return transparent;
+	}
+
+	/**
+	 * Sets the transparent
+	 * 
+	 * @param transparent the new transparent
+	 */
+	public void setTransparent(
+			boolean transparent) {
+		this.transparent = transparent;
+	}
+
+	/**
+	 * Returns the preventEventOverlap
+	 * 
+	 * @return the preventEventOverlap
+	 */
+	public boolean isPreventEventOverlap() {
+		return preventEventOverlap;
+	}
+
+	/**
+	 * Sets the preventEventOverlap
+	 * 
+	 * @param preventEventOverlap the new preventEventOverlap
+	 */
+	public void setPreventEventOverlap(
+			boolean preventEventOverlap) {
+		this.preventEventOverlap = preventEventOverlap;
+	}
+
 	/**
 	 * Constructs a new LuciferMapEventPage
 	 * 
@@ -153,35 +438,129 @@ public class LuciferMapEventPage {
 		}
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
-	public boolean equals(Object obj) {
-	     if (this == obj) {
-	        return true;
-	     }
-	     if (obj == null) {
-	        return false;
-	     }
-	     if (!(obj instanceof LuciferMapEventPage)) {
-	        return false; // different class
-	     }
-	     
-	     LuciferMapEventPage o = (LuciferMapEventPage) obj;
-	     
-	     return graphic == o.graphic
-	     		&& facing == o.facing
-	     		&& stepping == o.stepping
-	     		&& movementType == o.movementType
-	     		&& movementFrequency == o.movementFrequency
-	     		&& eventStartCondition == o.eventStartCondition
-	     		&& position == o.position
-	     		&& animationType == o.animationType
-	     		&& movementSpeed == o.movementSpeed
-	     		&& scriptLength == o.scriptLength
-	     		&& transparent == o.transparent
-	     		&& preventEventOverlap == o.preventEventOverlap
-	     		&& charset.equals(o.charset)
-	     		&& conditions.equals(o.conditions)
-	     		&& route.equals(o.route)
-	     		&& commands.equals(o.commands);
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result + (int) (animationType ^ (animationType >>> 32));
+		result = prime
+				* result + ((charset == null) ? 0
+						: charset.hashCode());
+		result = prime
+				* result + ((commands == null) ? 0
+						: commands.hashCode());
+		result = prime
+				* result + ((conditions == null) ? 0
+						: conditions.hashCode());
+		result = prime
+				* result + (int) (eventStartCondition ^ (eventStartCondition >>> 32));
+		result = prime
+				* result + (int) (facing ^ (facing >>> 32));
+		result = prime
+				* result + (int) (graphic ^ (graphic >>> 32));
+		result = prime
+				* result + (int) (movementFrequency ^ (movementFrequency >>> 32));
+		result = prime
+				* result + (int) (movementSpeed ^ (movementSpeed >>> 32));
+		result = prime
+				* result + (int) (movementType ^ (movementType >>> 32));
+		result = prime
+				* result + (int) (position ^ (position >>> 32));
+		result = prime
+				* result + (preventEventOverlap ? 1231
+						: 1237);
+		result = prime
+				* result + ((route == null) ? 0
+						: route.hashCode());
+		result = prime
+				* result + (int) (stepping ^ (stepping >>> 32));
+		result = prime
+				* result + (transparent ? 1231
+						: 1237);
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(
+			Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof LuciferMapEventPage)) {
+			return false;
+		}
+		LuciferMapEventPage other = (LuciferMapEventPage) obj;
+		if (animationType != other.animationType) {
+			return false;
+		}
+		if (charset == null) {
+			if (other.charset != null) {
+				return false;
+			}
+		} else if (!charset.equals(other.charset)) {
+			return false;
+		}
+		if (commands == null) {
+			if (other.commands != null) {
+				return false;
+			}
+		} else if (!commands.equals(other.commands)) {
+			return false;
+		}
+		if (conditions == null) {
+			if (other.conditions != null) {
+				return false;
+			}
+		} else if (!conditions.equals(other.conditions)) {
+			return false;
+		}
+		if (eventStartCondition != other.eventStartCondition) {
+			return false;
+		}
+		if (facing != other.facing) {
+			return false;
+		}
+		if (graphic != other.graphic) {
+			return false;
+		}
+		if (movementFrequency != other.movementFrequency) {
+			return false;
+		}
+		if (movementSpeed != other.movementSpeed) {
+			return false;
+		}
+		if (movementType != other.movementType) {
+			return false;
+		}
+		if (position != other.position) {
+			return false;
+		}
+		if (preventEventOverlap != other.preventEventOverlap) {
+			return false;
+		}
+		if (route == null) {
+			if (other.route != null) {
+				return false;
+			}
+		} else if (!route.equals(other.route)) {
+			return false;
+		}
+		if (stepping != other.stepping) {
+			return false;
+		}
+		if (transparent != other.transparent) {
+			return false;
+		}
+		return true;
 	}
 }
