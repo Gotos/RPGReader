138fd4ad432d96e2a22024d67382b92df65ecc65
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-07-31 22:45:13 +0200

    Work on #8 for LuciferMapTreeMap.

--- src/engine/LuciferMapTreeMap.java
+++ src/engine/LuciferMapTreeMap.java
@@ -11,24 +11,24 @@ import java.util.Arrays;
  */
 public class LuciferMapTreeMap{
 	
-	public long id;
-	public String name			= "";
-	public String battleBGName	= "";
-	public long parent			= 0;
-	public long depth			= 0;
-	public long type			= 1;
-	public long music			= 0;
-	public long xScrollBar		= 0;
-	public long yScrollBar		= 0;
-	public long battleBG		= 0;
-	public long teleport		= 0;
-	public long escape			= 0;
-	public long save			= 0;
-	public long encounterRate	= 25;
-	public boolean expanded	= false;
-	public long[] area			= new long[]{-1, -1, -1, -1};
-	public LuciferMusicUnit musicTrack;
-	public LuciferEncounterData encounter;
+	private long id;
+	private String name			= "";
+	private String battleBGName	= "";
+	private long parent			= 0;
+	private long depth			= 0;
+	private short type			= 1;
+	private short music			= 0;
+	private long xScrollBar		= 0;
+	private long yScrollBar		= 0;
+	private short battleBG		= 0;
+	private short teleport		= 0;
+	private long escape			= 0;
+	private long save			= 0;
+	private long encounterRate	= 25;
+	private boolean expanded	= false;
+	private long[] area			= new long[]{-1, -1, -1, -1};
+	private LuciferMusicUnit musicTrack;
+	private LuciferEncounterData encounter;
 	
 	/**
 	 * Constructs a new LuciferMapTreeMap
@@ -75,7 +75,7 @@ public class LuciferMapTreeMap{
 				//0 -> Root
 				//1 -> Map
 				//2 -> Area
-				type = DataReader.rpgintToInt(unit.content).integer;
+				type = (short) DataReader.rpgintToInt(unit.content).integer;
 				break;
 			case 0x5:
 				xScrollBar = DataReader.rpgintToInt(unit.content).integer;
@@ -90,7 +90,7 @@ public class LuciferMapTreeMap{
 				//0 -> use music of parent map
 				//1 -> don't change playing track
 				//2 -> use own track
-				music = DataReader.rpgintToInt(unit.content).integer;
+				music = (short) DataReader.rpgintToInt(unit.content).integer;
 				break;
 			case 0xC:
 				musicTrack = new LuciferMusicUnit(unit.content);
@@ -99,7 +99,7 @@ public class LuciferMapTreeMap{
 				//0 -> use bg of parent map
 				//1 -> use terrains bg
 				//2 -> use bg defined in 0x16
-				battleBG = DataReader.rpgintToInt(unit.content).integer;
+				battleBG = (short) DataReader.rpgintToInt(unit.content).integer;
 				break;
 			case 0x16:
 				battleBGName = new String(unit.content, Encoder.ENCODING);
@@ -108,19 +108,19 @@ public class LuciferMapTreeMap{
 				//0 -> teleport like parent map
 				//1 -> allow teleport
 				//2 -> disallow teleport
-				teleport = DataReader.rpgintToInt(unit.content).integer;
+				teleport = (short) DataReader.rpgintToInt(unit.content).integer;
 				break;
 			case 0x20:
 				//0 -> escape like parent map
 				//1 -> allow escape
 				//2 -> disallow escape
-				escape = DataReader.rpgintToInt(unit.content).integer;
+				escape = (short) DataReader.rpgintToInt(unit.content).integer;
 				break;
 			case 0x21:
 				//0 -> save like parent map
 				//1 -> allow save
 				//2 -> disallow save
-				save = DataReader.rpgintToInt(unit.content).integer;
+				save = (short) DataReader.rpgintToInt(unit.content).integer;
 				break;
 			case 0x29:
 				encounter = new LuciferEncounterData(unit.content);
@@ -143,6 +143,388 @@ public class LuciferMapTreeMap{
 		}
 	}
 	
+	/**
+	 * Returns the id
+	 * 
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 * 
+	 * @param id the new id
+	 */
+	public void setId(
+			long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Returns the name
+	 * 
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets the name
+	 * 
+	 * @param name the new name
+	 */
+	public void setName(
+			String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Returns the battleBGName
+	 * 
+	 * @return the battleBGName
+	 */
+	public String getBattleBGName() {
+		return battleBGName;
+	}
+
+	/**
+	 * Sets the battleBGName
+	 * 
+	 * @param battleBGName the new battleBGName
+	 */
+	public void setBattleBGName(
+			String battleBGName) {
+		this.battleBGName = battleBGName;
+	}
+
+	/**
+	 * Returns the parent
+	 * 
+	 * @return the parent
+	 */
+	public long getParent() {
+		return parent;
+	}
+
+	/**
+	 * Sets the parent
+	 * 
+	 * @param parent the new parent
+	 */
+	public void setParent(
+			long parent) {
+		this.parent = parent;
+	}
+
+	/**
+	 * Returns the depth
+	 * 
+	 * @return the depth
+	 */
+	public long getDepth() {
+		return depth;
+	}
+
+	/**
+	 * Sets the depth
+	 * 
+	 * @param depth the new depth
+	 */
+	public void setDepth(
+			long depth) {
+		this.depth = depth;
+	}
+
+	/**
+	 * Returns the type. Should be:
+	 * 		0 -> Root
+	 *		1 -> Map
+	 *		2 -> Area
+	 * 
+	 * @return the type
+	 */
+	public short getType() {
+		return type;
+	}
+
+	/**
+	 * Sets the type. Should be:
+	 * 		0 -> Root
+	 *		1 -> Map
+	 *		2 -> Area
+	 * 
+	 * @param type the new type
+	 */
+	public void setType(
+			short type) {
+		this.type = type;
+	}
+
+	/**
+	 * Returns the music. Should be:
+	 * 		0 -> use music of parent map
+	 *		1 -> don't change playing track
+	 *		2 -> use own track
+	 * 
+	 * @return the music
+	 */
+	public short getMusic() {
+		return music;
+	}
+
+	/**
+	 * Sets the music. Should be:
+	 * 		0 -> use music of parent map
+	 *		1 -> don't change playing track
+	 *		2 -> use own track
+	 * 
+	 * @param music the new music
+	 */
+	public void setMusic(
+			short music) {
+		this.music = music;
+	}
+
+	/**
+	 * Returns the xScrollBar
+	 * 
+	 * @return the xScrollBar
+	 */
+	public long getxScrollBar() {
+		return xScrollBar;
+	}
+
+	/**
+	 * Sets the xScrollBar
+	 * 
+	 * @param xScrollBar the new xScrollBar
+	 */
+	public void setxScrollBar(
+			long xScrollBar) {
+		this.xScrollBar = xScrollBar;
+	}
+
+	/**
+	 * Returns the yScrollBar
+	 * 
+	 * @return the yScrollBar
+	 */
+	public long getyScrollBar() {
+		return yScrollBar;
+	}
+
+	/**
+	 * Sets the yScrollBar
+	 * 
+	 * @param yScrollBar the new yScrollBar
+	 */
+	public void setyScrollBar(
+			long yScrollBar) {
+		this.yScrollBar = yScrollBar;
+	}
+
+	/**
+	 * Returns the battleBG. Should be:
+	 * 		0 -> use BG of parent map
+	 *		1 -> use terrains BG
+	 *		2 -> use own BG
+	 * 
+	 * @return the battleBG
+	 */
+	public short getBattleBG() {
+		return battleBG;
+	}
+
+	/**
+	 * Sets the battleBG. Should be:
+	 * 		0 -> use BG of parent map
+	 *		1 -> use terrains BG
+	 *		2 -> use own BG
+	 * 
+	 * @param battleBG the new battleBG
+	 */
+	public void setBattleBG(
+			short battleBG) {
+		this.battleBG = battleBG;
+	}
+
+	/**
+	 * Returns the teleport. Should be:
+	 * 		0 -> teleport like parent map
+	 *		1 -> allow teleport
+	 *		2 -> disallow teleport
+	 * 
+	 * @return the teleport
+	 */
+	public short getTeleport() {
+		return teleport;
+	}
+
+	/**
+	 * Sets the teleport. Should be:
+	 * 		0 -> teleport like parent map
+	 *		1 -> allow teleport
+	 *		2 -> disallow teleport
+	 * 
+	 * @param teleport the new teleport
+	 */
+	public void setTeleport(
+			short teleport) {
+		this.teleport = teleport;
+	}
+
+	/**
+	 * Returns the escape. Should be:
+	 * 		0 -> escape like parent map
+	 *		1 -> allow escape
+	 *		2 -> disallow escape
+	 * 
+	 * @return the escape
+	 */
+	public long getEscape() {
+		return escape;
+	}
+
+	/**
+	 * Sets the escape. Should be:
+	 * 		0 -> escape like parent map
+	 *		1 -> allow escape
+	 *		2 -> disallow escape
+	 * 
+	 * @param escape the new escape
+	 */
+	public void setEscape(
+			long escape) {
+		this.escape = escape;
+	}
+
+	/**
+	 * Returns the save. Should be:
+	 * 		0 -> save like parent map
+	 *		1 -> allow save
+	 *		2 -> disallow save
+	 * 
+	 * @return the save
+	 */
+	public long getSave() {
+		return save;
+	}
+
+	/**
+	 * Sets the save. Should be:
+	 * 		0 -> save like parent map
+	 *		1 -> allow save
+	 *		2 -> disallow save
+	 * 
+	 * @param save the new save
+	 */
+	public void setSave(
+			long save) {
+		this.save = save;
+	}
+
+	/**
+	 * Returns the encounterRate
+	 * 
+	 * @return the encounterRate
+	 */
+	public long getEncounterRate() {
+		return encounterRate;
+	}
+
+	/**
+	 * Sets the encounterRate
+	 * 
+	 * @param encounterRate the new encounterRate
+	 */
+	public void setEncounterRate(
+			long encounterRate) {
+		this.encounterRate = encounterRate;
+	}
+
+	/**
+	 * Returns the expanded
+	 * 
+	 * @return the expanded
+	 */
+	public boolean isExpanded() {
+		return expanded;
+	}
+
+	/**
+	 * Sets the expanded
+	 * 
+	 * @param expanded the new expanded
+	 */
+	public void setExpanded(
+			boolean expanded) {
+		this.expanded = expanded;
+	}
+
+	/**
+	 * Returns the area. The area is a lenght-4 array, where the first two entrys are the x and y coordinates of the upper
+	 * left corner and the secound two entries are the coordinates of the lower right corner, exclusive. E.g. 2,2,3,3
+	 * will only contain the field 2,2.
+	 * 
+	 * @return the area
+	 */
+	public long[] getArea() {
+		return area;
+	}
+
+	/**
+	 * Sets the area. The area is a lenght-4 array, where the first two entrys are the x and y coordinates of the upper
+	 * left corner and the secound two entries are the coordinates of the lower right corner, exclusive. E.g. 2,2,3,3
+	 * will only contain the field 2,2.
+	 * 
+	 * @param area the new area
+	 */
+	public void setArea(
+			long[] area) {
+		this.area = area;
+	}
+
+	/**
+	 * Returns the musicTrack
+	 * 
+	 * @return the musicTrack
+	 */
+	public LuciferMusicUnit getMusicTrack() {
+		return musicTrack;
+	}
+
+	/**
+	 * Sets the musicTrack
+	 * 
+	 * @param musicTrack the new musicTrack
+	 */
+	public void setMusicTrack(
+			LuciferMusicUnit musicTrack) {
+		this.musicTrack = musicTrack;
+	}
+
+	/**
+	 * Returns the encounter
+	 * 
+	 * @return the encounter
+	 */
+	public LuciferEncounterData getEncounter() {
+		return encounter;
+	}
+
+	/**
+	 * Sets the encounter
+	 * 
+	 * @param encounter the new encounter
+	 */
+	public void setEncounter(
+			LuciferEncounterData encounter) {
+		this.encounter = encounter;
+	}
+
 	/**
 	 * Returns the byte-representation of this MapTreeMap
 	 * 
