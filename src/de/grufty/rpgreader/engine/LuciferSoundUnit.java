e35ff63099af69503da95ce1347c9c068fd0f6f2
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-08-19 17:42:04 +0200

    Work on #20 for LuciferSoundUnit

--- src/de/grufty/rpgreader/engine/LuciferSoundUnit.java
+++ src/de/grufty/rpgreader/engine/LuciferSoundUnit.java
@@ -135,4 +135,58 @@ public class LuciferSoundUnit {
 		this.balance = balance;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result + (int) (balance ^ (balance >>> 32));
+		result = prime
+				* result + ((filename == null) ? 0
+						: filename.hashCode());
+		result = prime
+				* result + (int) (tempo ^ (tempo >>> 32));
+		result = prime
+				* result + (int) (volume ^ (volume >>> 32));
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(
+			Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof LuciferSoundUnit)) {
+			return false;
+		}
+		LuciferSoundUnit other = (LuciferSoundUnit) obj;
+		if (balance != other.balance) {
+			return false;
+		}
+		if (filename == null) {
+			if (other.filename != null) {
+				return false;
+			}
+		} else if (!filename.equals(other.filename)) {
+			return false;
+		}
+		if (tempo != other.tempo) {
+			return false;
+		}
+		if (volume != other.volume) {
+			return false;
+		}
+		return true;
+	}
+
 }
