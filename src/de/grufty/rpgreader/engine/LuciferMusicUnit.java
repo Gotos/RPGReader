fb950a006076534aa11fb658b147a79bc44d2f96
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-08-19 17:41:03 +0200

    Make LuciferMusicUnit extend LuciferSoundUnit, not the other way.

--- src/de/grufty/rpgreader/engine/LuciferMusicUnit.java
+++ src/de/grufty/rpgreader/engine/LuciferMusicUnit.java
@@ -8,13 +8,9 @@ import java.io.UnsupportedEncodingException;
  *
  * This class represents a Music-changer/-setter of the RPG-Maker-Game.
  */
-public class LuciferMusicUnit {
+public class LuciferMusicUnit extends LuciferSoundUnit {
 	
-	private String filename	= "";
-	private long fadeIn		= 0;
-	private long volume		= 100;
-	private long tempo		= 100;
-	private long balance	= 50;
+	private long fadeIn	= 0;
 	
 	/**
 	 * Constructs a new LuciferMusicUnit
@@ -44,52 +40,22 @@ public class LuciferMusicUnit {
 	private void init(DataReader sr) throws IOException {
 		LuciferBaseUnit unit = sr.nextUnitReadID();
 		while (unit.id != 0) {
-			switch (unit.id) {
-			case 0x01:
-				filename = new String(unit.content, Encoder.ENCODING);
-				break;
-			case 0x02:
-				//the fade-in time in sec*1000
-				fadeIn = DataReader.rpgintToInt(unit.content).integer;
-				break;
-			case 0x03:
-				volume = DataReader.rpgintToInt(unit.content).integer;
-				break;
-			case 0x04:
-				tempo = DataReader.rpgintToInt(unit.content).integer;
-				break;
-			case 0x05:
-				//0 -> only left
-				//50 -> middel
-				//100 -> only right
-				balance = DataReader.rpgintToInt(unit.content).integer;
-				break;
-			default:
-				Helper.warn(3, "Unknown Unit-ID in LuciferMusicUnit! ID: " + unit.id);
-			}
+			idSetter(unit);
 			unit = sr.nextUnit();
 		}
 	}
 	
-	/**
-	 * Returns the filename
-	 * 
-	 * @return the filename
-	 */
-	public String getFilename() {
-		return filename;
+	protected void idSetter(LuciferBaseUnit unit) throws IOException {
+		switch (unit.id) {
+		case 0x02:
+			//the fade-in time in sec*1000
+			fadeIn = DataReader.rpgintToInt(unit.content).integer;
+			break;
+		default:
+			super.idSetter(unit);
+		}
 	}
-
-	/**
-	 * Sets the filename
-	 * 
-	 * @param filename the new filename
-	 */
-	public void setFilename(
-			String filename) {
-		this.filename = filename;
-	}
-
+	
 	/**
 	 * Returns the fadeIn-time in sec/1000
 	 * 
@@ -107,63 +73,6 @@ public class LuciferMusicUnit {
 	public void setFadeIn(
 			long fadeIn) {
 		this.fadeIn = fadeIn;
-	}
-
-	/**
-	 * Returns the volume
-	 * 
-	 * @return the volume
-	 */
-	public long getVolume() {
-		return volume;
-	}
-
-	/**
-	 * Sets the volume
-	 * 
-	 * @param volume the new volume
-	 */
-	public void setVolume(
-			long volume) {
-		this.volume = volume;
-	}
-
-	/**
-	 * Returns the tempo
-	 * 
-	 * @return the tempo
-	 */
-	public long getTempo() {
-		return tempo;
-	}
-
-	/**
-	 * Sets the tempo
-	 * 
-	 * @param tempo the new tempo
-	 */
-	public void setTempo(
-			long tempo) {
-		this.tempo = tempo;
-	}
-
-	/**
-	 * Returns the balance. 0 is left, 50 balanced and 100 right.
-	 * 
-	 * @return the balance
-	 */
-	public long getBalance() {
-		return balance;
-	}
-
-	/**
-	 * Sets the balance. 0 is left, 50 balanced and 100 right.
-	 * 
-	 * @param balance the new balance
-	 */
-	public void setBalance(
-			long balance) {
-		this.balance = balance;
 	}
 
 	/**
--- src/de/grufty/rpgreader/engine/LuciferSoundUnit.java
+++ src/de/grufty/rpgreader/engine/LuciferSoundUnit.java
@@ -7,19 +7,132 @@ import java.io.IOException;
 //At the moment every LuciferSoundUnit has a fadeIn-value,
 //which will always be 0, but this might change.
 
-public class LuciferSoundUnit extends LuciferMusicUnit {
-
+public class LuciferSoundUnit {
+	
+	protected String filename	= "";
+	protected long volume		= 100;
+	protected long tempo		= 100;
+	protected long balance		= 50;
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7982423181303903422L;
 
 	public LuciferSoundUnit(byte[] str) throws IOException {
-		super(str);
+		init(new DataReader(str));
 	}
 
 	public LuciferSoundUnit(DataReader sr) throws IOException {
-		super(sr);
+		init(sr);
+	}
+	
+	public LuciferSoundUnit() { }
+	
+	private void init(DataReader sr) throws IOException {
+		LuciferBaseUnit unit = sr.nextUnitReadID();
+		while (unit.id != 0) {
+			idSetter(unit);
+			unit = sr.nextUnit();
+		}
+	}
+	
+	protected void idSetter(LuciferBaseUnit unit) throws IOException {
+		switch (unit.id) {
+		case 0x01:
+			filename = new String(unit.content, Encoder.ENCODING);
+			break;
+		case 0x03:
+			volume = DataReader.rpgintToInt(unit.content).integer;
+			break;
+		case 0x04:
+			tempo = DataReader.rpgintToInt(unit.content).integer;
+			break;
+		case 0x05:
+			//0 -> only left
+			//50 -> middel
+			//100 -> only right
+			balance = DataReader.rpgintToInt(unit.content).integer;
+			break;
+		default:
+			Helper.warn(3, "Unknown Unit-ID in LuciferMusicUnit! ID: " + unit.id);
+		}
+	}
+	
+	/**
+	 * Returns the filename
+	 * 
+	 * @return the filename
+	 */
+	public String getFilename() {
+		return filename;
+	}
+
+	/**
+	 * Sets the filename
+	 * 
+	 * @param filename the new filename
+	 */
+	public void setFilename(
+			String filename) {
+		this.filename = filename;
+	}
+
+	/**
+	 * Returns the volume
+	 * 
+	 * @return the volume
+	 */
+	public long getVolume() {
+		return volume;
+	}
+
+	/**
+	 * Sets the volume
+	 * 
+	 * @param volume the new volume
+	 */
+	public void setVolume(
+			long volume) {
+		this.volume = volume;
+	}
+
+	/**
+	 * Returns the tempo
+	 * 
+	 * @return the tempo
+	 */
+	public long getTempo() {
+		return tempo;
+	}
+
+	/**
+	 * Sets the tempo
+	 * 
+	 * @param tempo the new tempo
+	 */
+	public void setTempo(
+			long tempo) {
+		this.tempo = tempo;
+	}
+
+	/**
+	 * Returns the balance. 0 is left, 50 balanced and 100 right.
+	 * 
+	 * @return the balance
+	 */
+	public long getBalance() {
+		return balance;
+	}
+
+	/**
+	 * Sets the balance. 0 is left, 50 balanced and 100 right.
+	 * 
+	 * @param balance the new balance
+	 */
+	public void setBalance(
+			long balance) {
+		this.balance = balance;
 	}
 
 }
