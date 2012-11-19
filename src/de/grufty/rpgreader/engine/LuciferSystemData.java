1a86290a7f1c11a52ff4a468cf2365b1b04ffe98
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-11-19 21:28:20 +0100

    Add missing breaks

--- src/de/grufty/rpgreader/engine/LuciferSystemData.java
+++ src/de/grufty/rpgreader/engine/LuciferSystemData.java
@@ -57,82 +57,119 @@ public class LuciferSystemData {
 			switch (unit.id) {
 			case 0x0B:
 				ship = new String(unit.content, Encoder.ENCODING);
+				break;
 			case 0x0C:
 				boat = new String(unit.content, Encoder.ENCODING);
+				break;
 			case 0x0D:
 				airship = new String(unit.content, Encoder.ENCODING);
+				break;
 			case 0x0E:
 				shipIndex = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
 			case 0x0F:
 				boatIndex = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
 			case 0x10:
 				airshipIndex = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
 			case 0x11:
 				titleGraphic = new String(unit.content, Encoder.ENCODING);
+				break;
 			case 0x12:
 				goGraphic = new String(unit.content, Encoder.ENCODING);
+				break;
 			case 0x13:
 				system = new String(unit.content, Encoder.ENCODING);
+				break;
 			case 0x15:
 				nrHeroes = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
 			case 0x16:
 				heroes = new ArrayList<Long>(nrHeroes);
 				DataReader subDr = new DataReader(unit.content);
 				for (int i = 0; i < nrHeroes; i++) {
 					heroes.add(subDr.nextInt());
 				}
-				case 0x1F:
-					titleMusic = new LuciferMusicUnit(unit.content);
-				case 0x20:
-					battleMusic = new LuciferMusicUnit(unit.content);
-				case 0x21:
-					battleEndMusic = new LuciferMusicUnit(unit.content);
-				case 0x22:
-					innMusic = new LuciferMusicUnit(unit.content);
-				case 0x23:
-					boatMusic = new LuciferMusicUnit(unit.content);
-				case 0x24:
-					shipMusic = new LuciferMusicUnit(unit.content);
-				case 0x25:
-					airshipMusic = new LuciferMusicUnit(unit.content);
-				case 0x26:
-					gameOverMusic = new LuciferMusicUnit(unit.content);
-				case 0x29:
-					cursorSE = new LuciferSoundUnit(unit.content);
-				case 0x2A:
-					decisionSE = new LuciferSoundUnit(unit.content);
-				case 0x2B:
-					cancelSE = new LuciferSoundUnit(unit.content);
-				case 0x2C:
-					buzzerSE = new LuciferSoundUnit(unit.content);
-				case 0x2D:
-					startBattleSE = new LuciferSoundUnit(unit.content);
-				case 0x2E:
-					escapeSE = new LuciferSoundUnit(unit.content);
-				case 0x2F:
-					enemyAttackSE = new LuciferSoundUnit(unit.content);
-				case 0x30:
-					enemysDamageSE = new LuciferSoundUnit(unit.content);
-				case 0x31:
-					friendsDamageSE = new LuciferSoundUnit(unit.content);
-				case 0x32:
-					evasionSE = new LuciferSoundUnit(unit.content);
-				case 0x33:
-					enemyDieSE = new LuciferSoundUnit(unit.content);
-				case 0x34:
-					itemUseSE = new LuciferSoundUnit(unit.content);
-				case 0x3D:
-					teleportErase = (short) DataReader.rpgintToInt(unit.content).integer;
-				case 0x3E:
-					teleportShow = (short) DataReader.rpgintToInt(unit.content).integer;
-				case 0x3F:
-					battleErase = (short) DataReader.rpgintToInt(unit.content).integer;
-				case 0x40:
-					battleShow = (short) DataReader.rpgintToInt(unit.content).integer;
-				case 0x41:
-					battleEndErase = (short) DataReader.rpgintToInt(unit.content).integer;
-				case 0x42:
-					battleEndShow = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
+			case 0x1F:
+				titleMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x20:
+				battleMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x21:
+				battleEndMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x22:
+				innMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x23:
+				boatMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x24:
+				shipMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x25:
+				airshipMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x26:
+				gameOverMusic = new LuciferMusicUnit(unit.content);
+				break;
+			case 0x29:
+				cursorSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x2A:
+				decisionSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x2B:
+				cancelSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x2C:
+				buzzerSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x2D:
+				startBattleSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x2E:
+				escapeSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x2F:
+				enemyAttackSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x30:
+				enemysDamageSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x31:
+				friendsDamageSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x32:
+				evasionSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x33:
+				enemyDieSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x34:
+				itemUseSE = new LuciferSoundUnit(unit.content);
+				break;
+			case 0x3D:
+				teleportErase = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
+			case 0x3E:
+				teleportShow = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
+			case 0x3F:
+				battleErase = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
+			case 0x40:
+				battleShow = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
+			case 0x41:
+				battleEndErase = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
+			case 0x42:
+				battleEndShow = (short) DataReader.rpgintToInt(unit.content).integer;
+				break;
 			default:
 				Helper.warn(3, "Unknown Unit-ID in LuciferSystemData! ID: " + unit.id);
 			}
