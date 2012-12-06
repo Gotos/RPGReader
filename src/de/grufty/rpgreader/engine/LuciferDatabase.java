7fa4733fac52b03addf78c933743546bac0b1d34
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-12-06 20:58:25 +0100

    Rework to ArrayLists.

--- src/de/grufty/rpgreader/engine/LuciferDatabase.java
+++ src/de/grufty/rpgreader/engine/LuciferDatabase.java
@@ -1,23 +1,31 @@
 package de.grufty.rpgreader.engine;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+/**
+ * @author alina
+ * 
+ * This class represents the Database of the RPG-Maker-Game.
+ * 
+ */
 public class LuciferDatabase {
 	
-	public LuciferHeroUnit[] heroes;
-	public LuciferSkillUnit[] skills;
-	public LuciferItemUnit[] items;
-	public LuciferMonsterUnit[] monsters;
-	public LuciferMonsterParty[] monsterParties;
-	public LuciferTerrainUnit[] terrains;
-	public LuciferAttributeUnit[] attributes;
-	public LuciferConditionUnit[] conditions;
-	public LuciferAnimationUnit[] animations;
-	public LuciferChipsetData[] chipsets;
+	public ArrayList<LuciferHeroUnit> heroes;
+	public ArrayList<LuciferSkillUnit> skills;
+	public ArrayList<LuciferItemUnit> items;
+	public ArrayList<LuciferMonsterUnit> monsters;
+	public ArrayList<LuciferMonsterParty> monsterParties;
+	public ArrayList<LuciferTerrainUnit> terrains;
+	public ArrayList<LuciferAttributeUnit> attributes;
+	public ArrayList<LuciferConditionUnit> conditions;
+	public ArrayList<LuciferAnimationUnit> animations;
+	public ArrayList<LuciferChipsetData> chipsets;
 	public LuciferSystemData system;
-	public String[] switchNames;
-	public String[] variableNames;
-	public LuciferCommonEvent[] commonEvents;
+	public ArrayList<String> switchNames;
+	public ArrayList<String> variableNames;
+	public ArrayList<LuciferCommonEvent> commonEvents;
+	public byte[] vocab;
 	
 	public LuciferDatabase(byte[] str) throws IOException {
 		init(new DataReader(str));
@@ -31,110 +39,138 @@ public class LuciferDatabase {
 		LuciferBaseUnit unit = sr.nextUnit();
 		while (unit.id != 0) {
 			DataReader tmp = new DataReader(unit.content);
+			int size;
 			switch(unit.id) {
 			case 0x0B:
-				heroes = new LuciferHeroUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < heroes.length; i++) {
-					tmp.nextInt(); //read id of hero
-					heroes[i] = new LuciferHeroUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				heroes = new ArrayList<LuciferHeroUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (heroes.size() < id) { heroes.add(null); }
+					heroes.add((int) id, new LuciferHeroUnit(tmp));
 				}
 				break;
 			case 0x0C:
-				skills = new LuciferSkillUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < skills.length; i++) {
-					tmp.nextInt(); //read id of skill
-					skills[i] = new LuciferSkillUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				skills = new ArrayList<LuciferSkillUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (skills.size() < id) { skills.add(null); }
+					skills.add((int) id, new LuciferSkillUnit(tmp));
 				}
 				break;
 			case 0x0D:
-				items = new LuciferItemUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < items.length; i++) {
-					tmp.nextInt(); //read id of item
-					items[i] = new LuciferItemUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				items = new ArrayList<LuciferItemUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (items.size() < id) { items.add(null); }
+					items.add((int) id, new LuciferItemUnit(tmp));
 				}
 				break;
 			case 0x0E:
-				monsters = new LuciferMonsterUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < monsters.length; i++) {
-					tmp.nextInt(); //read id of monster
-					monsters[i] = new LuciferMonsterUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				monsters = new ArrayList<LuciferMonsterUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (monsters.size() < id) { monsters.add(null); }
+					monsters.add((int) id, new LuciferMonsterUnit(tmp));
 				}
 				break;
 			case 0x0F:
-				monsterParties = new LuciferMonsterParty[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < monsterParties.length; i++) {
-					i = (int) tmp.nextInt(); //read id of monsterparty
-					monsterParties[i] = new LuciferMonsterParty(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				monsterParties = new ArrayList<LuciferMonsterParty>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (monsterParties.size() < id) { monsterParties.add(null); }
+					monsterParties.add((int) id, new LuciferMonsterParty(tmp));
 				}
-				break;
 			case 0x10:
-				terrains = new LuciferTerrainUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < terrains.length; i++) {
-					i = (int) tmp.nextInt(); //read id of terrain
-					terrains[i] = new LuciferTerrainUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				terrains = new ArrayList<LuciferTerrainUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (terrains.size() < id) { terrains.add(null); }
+					terrains.add((int) id, new LuciferTerrainUnit(tmp));
 				}
 				break;
 			case 0x11:
-				attributes = new LuciferAttributeUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < attributes.length; i++) {
-					i = (int) tmp.nextInt(); //read id of attribute
-					attributes[i] = new LuciferAttributeUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				attributes = new ArrayList<LuciferAttributeUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (attributes.size() < id) { attributes.add(null); }
+					attributes.add((int) id, new LuciferAttributeUnit(tmp));
 				}
 				break;
 			case 0x12:
-				conditions = new LuciferConditionUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < conditions.length; i++) {
-					i = (int) tmp.nextInt(); //read id of attribute
-					conditions[i] = new LuciferConditionUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				conditions = new ArrayList<LuciferConditionUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (conditions.size() < id) { conditions.add(null); }
+					conditions.add((int) id, new LuciferConditionUnit(tmp));
 				}
-				break;
 			case 0x13:
-				//TODO: TEST THIS, BITCH!
-				animations = new LuciferAnimationUnit[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < animations.length; i++) {
-					i = (int) tmp.nextInt(); //read id of animation
-					animations[i] = new LuciferAnimationUnit(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				animations = new ArrayList<LuciferAnimationUnit>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (animations.size() < id) { animations.add(null); }
+					animations.add((int) id, new LuciferAnimationUnit(tmp));
 				}
 				break;
 			case 0x14:
-				chipsets = new LuciferChipsetData[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < chipsets.length; i++) {
-					i = (int) tmp.nextInt(); //read id of animation
-					chipsets[i] = new LuciferChipsetData(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				chipsets = new ArrayList<LuciferChipsetData>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (chipsets.size() < id) { chipsets.add(null); }
+					chipsets.add((int) id, new LuciferChipsetData(tmp));
 				}
+				break;
+			case 0x15:
+				//TODO: parse vocab
+				vocab = unit.content;
 				break;
 			case 0x16:
 				system = new LuciferSystemData(tmp);
 				break;
 			case 0x17:
-				switchNames = new String[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < switchNames.length; i++) {
+				size = (int) tmp.nextInt() + 1;
+				switchNames = new ArrayList<String>(size);
+				for (int i = 1; i < size; i++) {
 					i = (int) tmp.nextInt(); //read id of switch
-					if (i >= switchNames.length) { break; }
+					if (i >= size) { break; }
 					LuciferBaseUnit nameUnit = tmp.nextUnit();
-					switchNames[i] = new String(nameUnit.content, Encoder.ENCODING);
+					while (switchNames.size() < i) { switchNames.add(null); }
+					switchNames.add(i, new String(nameUnit.content, Encoder.ENCODING));
 					if (nameUnit.id != 0) {
 						tmp.nextInt(); //read 0x00
 					}
 				}
 				break;
 			case 0x18:
-				variableNames = new String[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < variableNames.length; i++) {
+				size = (int) tmp.nextInt() + 1;
+				variableNames = new ArrayList<String>(size);
+				for (int i = 1; i < size; i++) {
 					i = (int) tmp.nextInt(); //read id of switch
-					if (i >= variableNames.length) { break; }
+					if (i >= size) { break; }
 					LuciferBaseUnit nameUnit = tmp.nextUnit();
-					variableNames[i] = new String(nameUnit.content, Encoder.ENCODING);
+					while (variableNames.size() < i) { variableNames.add(null); }
+					variableNames.add(i, new String(nameUnit.content, Encoder.ENCODING));
 					if (nameUnit.id != 0) {
 						tmp.nextInt(); //read 0x00
 					}
 				}
 				break;
 			case 0x19:
-				commonEvents = new LuciferCommonEvent[(int) tmp.nextInt() + 1];
-				for (int i = 1; i < commonEvents.length; i++) {
-					i = (int) tmp.nextInt(); //read id of event
-					commonEvents[i] = new LuciferCommonEvent(tmp);
+				size = (int) tmp.nextInt(); //read nr
+				commonEvents = new ArrayList<LuciferCommonEvent>(size);
+				for (int i = 1; i < size; i++) {
+					int id = (int) tmp.nextInt(); //read id of hero
+					while (commonEvents.size() < id) { commonEvents.add(null); }
+					commonEvents.add((int) id, new LuciferCommonEvent(tmp));
 				}
 				break;
 			default:
