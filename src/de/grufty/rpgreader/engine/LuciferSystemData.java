d59d6e361ef8035a15bc19c40c6cbe58274abedd
Alina Saalfeld <alina.saalfeld@ymail.com>
2012-12-10 22:23:42 +0100

    Add write() and "implements UnitInterface" to System.

--- src/de/grufty/rpgreader/engine/LuciferSystemData.java
+++ src/de/grufty/rpgreader/engine/LuciferSystemData.java
@@ -1,9 +1,10 @@
 package de.grufty.rpgreader.engine;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-public class LuciferSystemData {
+public class LuciferSystemData implements UnitInterface {
 	
 	private String ship						= "";
 	private String boat						= "";
@@ -196,6 +197,71 @@ public class LuciferSystemData {
 				Helper.warn(3, "Unknown Unit-ID in LuciferSystemData! ID: " + unit.id + " " + new String(unit.content));
 			}
 			unit = dr.nextUnit();
+		}
+	}
+	
+	/**
+	 * Returns the byte-representation of this Monster
+	 * 
+	 * @return byte-representation
+	 */
+	public byte[] write() {
+		try {
+			byte[] heroArray = DataReader.intToRPGint(heroes.size());
+			for (int i = 0; i < heroes.size(); i++) {
+				if (heroes.get(i) != null) {
+					heroArray = Helper.concatAll(heroArray, DataReader.intToRPGint(i), DataReader.intToRPGint(heroes.get(i)));
+				}
+			}
+			
+			return Helper.concatAll(new LuciferBaseUnit(0x0B, ship.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x0C, boat.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x0D, airship.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x0E, DataReader.intToRPGint(shipIndex)).write(new byte[]{0}),
+					new LuciferBaseUnit(0x0F, DataReader.intToRPGint(boatIndex)).write(new byte[]{0}),
+					new LuciferBaseUnit(0x10, DataReader.intToRPGint(airshipIndex)).write(new byte[]{0}),
+					new LuciferBaseUnit(0x11, titleGraphic.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x12, goGraphic.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x13, system.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x15, DataReader.intToRPGint(heroes.size())).write(new byte[]{0}),
+					new LuciferBaseUnit(0x16, heroArray).write(new byte[0]),
+					new LuciferBaseUnit(0x1F, titleMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x20, battleMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x21, battleEndMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x22, innMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x23, boatMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x24, shipMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x25, airshipMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x26, gameOverMusic.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x29, cursorSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x2A, decisionSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x2B, cancelSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x2C, buzzerSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x2D, startBattleSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x2E, escapeSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x2F, enemyAttackSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x30, enemysDamageSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x31, friendsDamageSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x32, evasionSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x33, enemyDieSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x34, itemUseSE.write()).write(new byte[0]),
+					new LuciferBaseUnit(0x3D, DataReader.intToRPGint(teleportErase)).write(),
+					new LuciferBaseUnit(0x3E, DataReader.intToRPGint(teleportShow)).write(),
+					new LuciferBaseUnit(0x3F, DataReader.intToRPGint(battleErase)).write(),
+					new LuciferBaseUnit(0x40, DataReader.intToRPGint(battleShow)).write(),
+					new LuciferBaseUnit(0x41, DataReader.intToRPGint(battleEndErase)).write(),
+					new LuciferBaseUnit(0x42, DataReader.intToRPGint(battleEndShow)).write(),
+					//TODO: ID 47
+					//TODO: ID 48
+					new LuciferBaseUnit(0x51, DataReader.intToRPGint(bATestVictim)).write(new byte[]{0}),
+					new LuciferBaseUnit(0x52, DataReader.intToRPGint(bAUseGrid ? 1 : 0)).write(new byte[]{0}),
+					new LuciferBaseUnit(0x54, battleTestBG.getBytes(Encoder.ENCODING)).write(new byte[0]),
+					new LuciferBaseUnit(0x5B, DataReader.intToRPGint(numberOfSaves)).write(new byte[]{0}),
+					new byte[]{0}
+					);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return new byte[0];
 		}
 	}
 
